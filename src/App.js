4fc6815a4f26be2fab6b80962bef93ebd5afc093
Hennadii Vasylchenko <vasil@mfdoors.com.ua>
2022-11-07 15:14:48 +0200

    fetch_create_filter_sort

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
@@ -8,33 +8,11 @@ import "./styles/App.css";
 import { usePosts } from "./components/hoocks/usePosts";
 import PostService from "../src/API/PostService";
 import BikeLoader from "./components/UI/loader/BikeLoader";
-// import Loader from "./components/UI/loader/Loader";
 import { useFetching } from "./components/hoocks/useFetching";
 import { getPageCout } from "./utils/page.jsx";
-// import City from "./components/City";
-// import CitiesList from "./components/CitiesList";
-import ClassCounter from "./components/ClassCounter";
 
 function App() {
-  // const [cities, setCities] = useState([
-  //   { name: "London", description: "is the capital of GB" },
-  //   { name: "Kyiv", description: "is the capital of Ukraine" },
-  // ]);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
   const [posts, setPosts] = useState([]);
-  // const [jetlms, setJetlms] = useState({ name: "user", password: "0123" });
-  // const [jetlms, setJetlms] = useState("0123");
-  // const changePassword = () => {
-  //   setJetlms({ name: "user", password: "01234567" });
-  //   console.log("jetlms", jetlms);
-  // };
-
-  // const [posts, setPosts] = useState([
-  //   { id: "1", title: "Javascript", body: "Javascript - programming language" },
-  //   { id: "2", title: "Java", body: "Java - programming language" },
-  //   { id: "3", title: "Pyton", body: "Pyton - programming language" },
-  // ]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
@@ -48,64 +26,34 @@ function App() {
     const totalCount = response.headers["x-total-count"];
     setTotalPages(getPageCout(totalCount, limit));
   });
-  // console.log(totalPages);
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
-  // async function fetchPosts() {
-  //   setIsPostsLoading(true);
-  //   const posts = await PostService.getAll();
-  //   setTimeout(() => {
-  //     setPosts(posts);
-  //     setIsPostsLoading(false);
-  //   }, 5000);
-  // };
-
-  // useEffect(() => {
-  //   fetchPosts();
-  // }, []);
-
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
-  // const handlerChangeCity = (description) => {
-  //   setCities(
-  //     cities.map((city, index) => {
-  //       if (index === currentIndex) {
-  //         return { ...city, description: description };
-  //       }
-  //       return city;
-  //     })
-  //   );
-  // };
-  // const handlerSelectCity = (currentIndex) => {
-  //   setCurrentIndex(currentIndex);
-  // };
-
   return (
     <div className="App">
-      {/* <ClassCounter/> */}
-      {/* <City city={cities[currentIndex]} onChangeCity={handlerChangeCity} />
-      <CitiesList cities={cities} onSelectCity={handlerSelectCity} /> */}
-
-      {/* <button onClick={changePassword}>{jetlms.name}</button> */}
       <MyButton style={{ marginTop: "30px" }} onClick={fetchPosts}>
         Get posts from https://jsonplaceholder.typicode.com/posts
       </MyButton>
-      <MyButton style={{ marginTop: "30px", marginLeft: "20px" }} onClick={() => setModal(true)}>
+      <MyButton
+        style={{ marginTop: "30px", marginLeft: "20px" }}
+        onClick={() => setModal(true)}
+      >
         Create post
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && 
-      <h1>Mistake has happened {postError}</h1>}
+      {postError && <h1>Mistake has happened {postError}</h1>}
       {isPostsLoading ? (
-        <BikeLoader/>
+        <BikeLoader />
       ) : (
         <PostList
           remove={removePost}
